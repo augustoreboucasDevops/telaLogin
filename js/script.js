13a53e9e54e93d4558dd728aeb07e9de9433a3d9
thiagoReboucas <reboucasaugust@gmail.com>
2023-12-18 22:00:20 -0300

    tela de acesso

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector(".input__field");
+const inputIcon = document.querySelector(".input__icon");
+
+inputIcon.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    inputIcon.setAttribute(
+        'src', 
+        input.getAttribute('type') === 'password' ?
+        'image/eye-off.svg'
+          :
+        'image/eye.svg'
+    );
+
+    input.setAttribute(
+        'type', 
+        input.getAttribute('type') === 'password' ? 
+        'text'
+          :
+        'password'
+    );
+});
